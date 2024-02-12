dfb91e030bf0bde20d9fe1a8d385b499e4e1b454
Chris <almeidachristian94@hotmail.com>
2024-02-12 15:57:27 -0300

    Add ability to select sections of the board
    Figuring out the logic of the game by trying it out in console. The playGame object counts the turns and informs the gameboard object the position to update and with what player.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 const gameboard = (function(){
     const board = [[],[]];
     const SIZE = 3;
+    const listeners = [];
     const initialize = () => {
         for(let i = 0; i < SIZE; i++)
         {
@@ -8,6 +9,11 @@ const gameboard = (function(){
             for(let j = 0; j < SIZE; j++)
                 board[i][j] = 0;
         }
+    }
+    const liveGame = () => {
+        for(let i = 0; i < 9; i++)
+            listeners[i] = document.getElementById(i);
+        return listeners;
     }
     const updateBoard = (row,column,player) =>{
         if(board[row][column] !== 0)
@@ -29,7 +35,76 @@ const gameboard = (function(){
         }
     }
 
-    return {initialize, updateBoard, printBoard};
+    return {initialize, updateBoard, printBoard, liveGame};
 })();
+
+function player(names, Number)
+{
+    const name = names;
+    let score = 0;
+    const playerNumber = Number;
+    const getName = ()=> name;
+    const getScore = ()=> score;
+    const getPlayerNumber = ()=> playerNumber;
+    const registerWin = () => score++;
+
+    return {getName,getScore,getPlayerNumber,registerWin};
+}
+
+const playGame = (function (){
+    gameboard.initialize();
+    const playerOne = player('one',1);
+    const playerTwo = player('two',2);
+    let turn = 1;
+    const whichTurn = () => {
+        if(turn % 2 === 0)
+        {
+            turn++;
+            return 2;
+        }
+        else
+        {
+            turn++;
+            return 1;
+        }
+    };
+    const playTheGame = () => {
+        const buttons = gameboard.liveGame();
+        for(let i = 0; i < 9; i++)
+            {buttons[i].addEventListener('click',()=>{
+                
+                switch(i){
+                    case 0:
+                        gameboard.updateBoard(0,0,whichTurn());
+                        break;
+                    case 1: gameboard.updateBoard(0,1,whichTurn());
+                    break;
+                    case 2:
+                        gameboard.updateBoard(0,2,whichTurn());
+                        break;
+                    case 3: gameboard.updateBoard(1,0,whichTurn());
+                    break;
+                    case 4:
+                        gameboard.updateBoard(1,1,whichTurn());
+                        break;
+                    case 5: gameboard.updateBoard(1,2,whichTurn());
+                    break;
+                    case 6:
+                        gameboard.updateBoard(2,0,whichTurn());
+                        break;
+                    case 7: gameboard.updateBoard(2,1,whichTurn());
+                    break;
+                    case 8: gameboard.updateBoard(2,2,whichTurn());
+                    break;
+                }
+                gameboard.printBoard();
+            })
+        }
+    }
+    return {playerOne,playerTwo,whichTurn,playTheGame};
+})();
+
+
+playGame.playTheGame();
 
 //remember the parenthesis
